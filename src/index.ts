357c8ddb360e265d2d680ac98fd98f35de3b90d2
tetra <me@tetra.cool>
2022-03-06 23:41:42 -0500

    add more error types to MyroError

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import Bot from "./components/bot.ts";
-import MyroError from "./components/myroerror.ts";
+import MyroError, { MyroErrorLevel } from "./components/myroerror.ts";
 import Downloader from "./components/downloader.ts";
 
-// load environment variables from .env file
+// Load environment variables from .env file
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
 let token = Deno.env.get("BOT_TOKEN_DEV");
 
 if (!token) {
-  throw new MyroError({
-    message: "BOT_TOKEN was expected in the environment",
-  });
+    new MyroError({
+        message: "BOT_TOKEN was expected in the environment",
+        level: MyroErrorLevel.FATAL
+    });
 }
 
 await Downloader.getExecutable();
 
-await Bot.init(token).catch((err) => console.error(err));
+await Bot.init(token!).catch((err) => console.error(err));
